b630b6da52e24624792340929226f79e8ac3a11d
Drew Sirenko <andrewsirenko@gmail.com>
2024-08-04 23:14:18 -0400

    Add user token activation

--- internal/data/models.go
+++ internal/data/models.go
@@ -12,12 +12,14 @@ var (
 
 type Models struct {
 	Movies MovieModel
+	Tokens TokenModel // Add a new Tokens field.
 	Users  UserModel
 }
 
 func NewModels(db *sql.DB) Models {
 	return Models{
 		Movies: MovieModel{DB: db},
+		Tokens: TokenModel{DB: db}, // Initialize a new TokenModel instance.
 		Users:  UserModel{DB: db},
 	}
 }
